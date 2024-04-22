1881926a0c3006fa5557842b042c9f985d552e4b
LionyRamli <lionyramli07@gmail.com>
2024-04-22 22:32:41 +0700

    c

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,10 @@
     @include('layouts.navbar')
     {{-- End Navbar --}}
 
+    {{-- Carousel --}}
+    @include('layouts.carousel')
+    {{-- End Navbar --}}
+
     {{-- Bootsrap JS --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-dark">
     <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="{{ asset('asset/img/FDX-remove.png') }}" alt="Logo FDX" width="100" height="60" class="d-inline-block align-text-top mr-2">
-            <img src="{{ asset('asset/img/YAMADALUBE.jpg') }}" alt="Logo YAMADALUBE" width="200" height="50" class="d-inline-block align-text-top">
+            <img src="{{ asset('asset/img/FDX-remove.png') }}" alt="Logo FDX" width="80" height="40" class="d-inline-block align-text-top mr-2">
+            <img src="{{ asset('asset/img/YAMADALUBE.jpg') }}" alt="Logo YAMADALUBE" width="150" height="30" class="d-inline-block align-text-top">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -10,13 +10,13 @@
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active text-white fs-5 mx-5" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active text-white fs-6 mx-5" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link mr-2 text-white fs-5 mx-5" href="#">Produk</a>
+                    <a class="nav-link mr-2 text-white fs-6 mx-5" href="#">Produk</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white fs-5 mx-5" href="#">Kontak</a>
+                    <a class="nav-link text-white fs-6 mx-5" href="#">Kontak</a>
                 </li>
             </ul>            
         </div>
