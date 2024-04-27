b4d51568fb0672cdf59f8524cc6a3b17437b6c33
LionyRamli <lionyramli07@gmail.com>
2024-04-27 20:13:44 +0700

    u

--- resources/views/layouts/carousel.blade.php
+++ resources/views/layouts/carousel.blade.php
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
+
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://dfcm824dmlg8u.cloudfront.net/wp-content/uploads/2021/10/0510-COVER-fakta-oto-01-01.jpg  " class="d-block w-100" alt="...">
-=======
+     
 <section class="home" id="home">
     <div class="swiper home-slider">
         <div class="swiper-wrapper">
@@ -42,6 +41,6 @@
         </div>
         {{-- <div class="swiper-button-next"></div>
         <div class="swiper-button-prev"></div> --}}
->>>>>>> cd133a1fa4e4505f1be0b54697d853b952d613df
+
     </div>
 </section>
