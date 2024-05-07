4bf3c2b44878261dcf601e13e478da1c704b85f4
LionyRamli <lionyramli07@gmail.com>
2024-05-07 08:54:15 +0700

    footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,6 +42,10 @@
     @include('layouts.produk')
     {{-- End Product --}}
 
+    {{-- Product --}}
+    @include('layouts.footer')
+    {{-- End Product --}}
+
     {{-- Bootstrap Bundle with Popper for Bootstrap JavaScript --}}
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,39 @@
+
+<section class="footer">
+        <div class="footer-content">
+            <a href="#" class="logo">TZU CHI <span>CORP</span></a>
+            <p>Terimakasih atas support dari kalian semoga<br>
+                konten saya bisa bermanfaat buat kalian.</p>
+            <div class="icon">
+                <a href="#"><i class='bx bxl-tiktok'></i></a>
+                <a href="#"><i class='bx bxl-instagram-alt'></i></a>
+                <a href="#"><i class='bx bxl-google'></i></a>
+                <a href="#"><i class='bx bxl-twitter'></i></a>
+            </div>
+        </div>
+
+        <div class="footer-content">
+            <h4>Company</h4>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Service</a></li>
+            <li><a href="#">Tutorial</a></li>
+        </div>
+
+        <div class="footer-content">
+            <h4>About</h4>
+            <li><a href="#">Karir</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Tentang</a></li>
+            <li><a href="#">Kontak Kami</a></li>
+        </div>
+
+        <div class="footer-content">
+            <h4>Service</h4>
+            <li><a href="#">Domain</a></li>
+            <li><a href="#">Transfer</a></li>
+            <li><a href="#">Website</a></li>
+            <li><a href="#">Bantuan</a></li>
+        </div>
+
+    </section>
